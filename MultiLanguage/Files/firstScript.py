ec1dd3c48fbada5ba20db21d9ec5e083365c80d4
silence <tianyun1201@163.com>
2020-10-28 17:18:26 +0800

    调整脚本

--- MultiLanguage/Files/firstScript.py
+++ MultiLanguage/Files/firstScript.py
@@ -34,6 +34,7 @@ if os.path.isdir(workPath):
 else:
     sys.exit(0)
 
+# 遍历工作目录，将所有需要过滤的文件筛选出来
 needHandleFilesArr = set()
 for root, dirs, files in os.walk(workPath, topdown=False):
     for file in files:
@@ -43,10 +44,47 @@ for root, dirs, files in os.walk(workPath, topdown=False):
             needHandleFilesArr.add(os.path.join(root, file))
         elif file.endswith('.mm'):
             needHandleFilesArr.add(os.path.join(root, file))
+        elif file.endswith('.xib'):
+            needHandleFilesArr.add(os.path.join(root, file))
+        elif file.endswith('.storyBoard'):
+            needHandleFilesArr.add(os.path.join(root, file))
         else:
             print("忽略" + os.path.join( file ))
 
-print ("\n除去.png、.jpg、.json、.DS_Store、.xib、.storyBoard等文件后，共%d份文件需要处理\n" % (len(needHandleFilesArr)))
+print ("\n只过滤.h .m .mm .xib .storyBoard中的中文生成映射，共%d份文件需要处理\n" % (len(needHandleFilesArr)))
+
+# 遍历所有需要筛选的文件，将满足条件的字符串保存到字典中，重名暂时不做区分（特定的翻译数量较少，出现这种情况可以手动再映射表里添加一项）
+multiDic = {}
+for filePath in needHandleFilesArr:#循环文件
+
+	totalString = get_contends(filePath)
+	stringArr = get_contends_arr(totalString)
+	print ("正则读取过滤" + filePath)
+	shouldReWrite = 0
+
+	if filePath.endswith('.xib') or filePath.endswith('.storyBoard'):
+		#xib 和 storyBoard 中的过滤
+		for lineSring in stringArr:#循环每一行内容
+			resultArr = re.findall('="[^"]*[\u4e00-\u9fa5]+.*?"', lineSring, re.U)
+			for targetStr in resultArr:
+				multiDic[targetStr[2:-1]] = {'key':targetStr[2:-1] , 'zh_CN':targetStr[2:-1] , 'en_GB':'' , 'ja_JP':''}
+	else: 
+		for lineSring in stringArr:#循环每一行内容
+			resultArr = re.findall('@"[^"]*[\u4e00-\u9fa5]+.*?"', lineSring, re.U)
+			for targetStr in resultArr:
+				if  len(targetStr) > 0:
+					#将文字写入json文件
+					multiDic[targetStr[2:-1]] = {'key':targetStr[2:-1] , 'zh_CN':targetStr[2:-1] , 'en_GB':'' , 'ja_JP':''}
+					newlineStr = lineSring.replace(targetStr,targetStr + ".multi")
+					#替换该行内容
+					totalString = totalString.replace(lineSring,newlineStr)
+					totalString = totalString.replace(".multi.multi",".multi")
+					shouldReWrite = 1
+	if shouldReWrite:
+		print ("替换写入" + filePath)
+	with open(filePath, 'w') as file_object:
+		file_object.write(totalString)
+		file_object.close()    
 
 
 #创建表格文件
@@ -56,31 +94,9 @@ print ("\n除去.png、.jpg、.json、.DS_Store、.xib、.storyBoard等文件后
 csvFile = open('/Users/silence/DeskTop/multi-language.csv','w',encoding='utf-8-sig',newline="")
 csv_writer = csv.writer(csvFile)
 csv_writer.writerow(["key","zh_CN","en_GB","ja_JP"])
-
-
-for filePath in needHandleFilesArr:#循环文件
-
-    totalString = get_contends(filePath)
-    stringArr = get_contends_arr(totalString)
-    print ("正则读取过滤" + filePath)
-    shouldReWrite = 0
-
-    for lineSring in stringArr:#循环每一行内容
-        resultArr = re.findall('@"[^"]*[\u4e00-\u9fa5]+.*?"', lineSring, re.U)
-        for targetStr in resultArr:
-            if  len(targetStr) > 0:#将文字写入json文件
-                csv_writer.writerow([targetStr[2:-1],targetStr[2:-1],'',''])
-                newlineStr = lineSring.replace(targetStr,targetStr + ".multi")
-                #替换该行内容
-                totalString = totalString.replace(lineSring,newlineStr)
-                totalString = totalString.replace(".multi.multi",".multi")
-                shouldReWrite = 1
-    if shouldReWrite:
-        print ("正则替换写入" + filePath)
-        with open(filePath, 'w') as file_object:
-            file_object.write(totalString)
-            file_object.close()
-
+# 4. 将字典写入表格文件
+for key in multiDic.keys():#循环每一行内容
+	csv_writer.writerow([key,key,'',''])
 # 5. 关闭文件
 csvFile.close()
 print ("处理完毕生成scv表格文件")
