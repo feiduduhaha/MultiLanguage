286f890395ee23481177208123b598e0f03c3006
silence <tianyun1201@163.com>
2020-11-18 15:44:35 +0800

    按钮多状态的映射

--- MultiLanguage/Classes/UIView+multi_language.m
+++ MultiLanguage/Classes/UIView+multi_language.m
@@ -22,14 +22,17 @@
     if ([self isKindOfClass:[UIButton class]]) {
         
         UIButton * btn = (UIButton *)self;
-        [btn setTitle:btn.currentTitle.multi forState:btn.state];
+        [self configTitleForButton:btn state:UIControlStateNormal];
+        [self configTitleForButton:btn state:UIControlStateHighlighted];
+        [self configTitleForButton:btn state:UIControlStateDisabled];
+        [self configTitleForButton:btn state:UIControlStateSelected];
     }
     else if ([self isKindOfClass:[UITextField class]]){
         
         UITextField * textField = (UITextField *)self;
         textField.placeholder = textField.placeholder.multi;
 //        if (textField.text.length) {
-//            
+//
 //            textField.text = textField.text.multi;
 //        }
     }
@@ -40,6 +43,14 @@
             
             item.title = item.title.multi;
         }
+    }
+}
+
+- (void)configTitleForButton:(UIButton *)button state:(UIControlState)state
+{
+    NSString *title = [button titleForState:state];
+    if (title) {
+        [button setTitle:title.multi forState:state];
     }
 }
 
